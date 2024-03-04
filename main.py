18bfa13aeba143326e52ce01b579e9c0d2c73aac
fantare <49351714+fantare@users.noreply.github.com>
2024-03-04 13:34:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,  HTTPException
 from fastapi.responses import JSONResponse
 import pandas as pd
+from typing import Annotated
 import shutil
 import tempfile
 import os
-from typing import Annotated
+import sys
+import time
+import asyncio
+import json
 from qvd import qvd_reader
 
 #put response codes eventually
@@ -12,44 +16,71 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World"}    
 
-#wrap in if statemnet for if qvd file, else response for incorrect file..........
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
+    try:
+        return qvd_to_json(file)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=f"ValueError: {ve}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+def qvd_to_json(file:UploadFile):
     print(f"Received file: {file.filename}")
     df_read = qvd_reader.read(file.filename)
     df = pd.DataFrame(df_read)
         
     json_data = df.to_json(orient='index') ##df.to_json(r'Path to store the exported JSON file\File Name.json')
 
-    return {json_data}
-
-async def json_to_powerbi():
+    return json_data
 
 
-#need to have the json_data hit power bi's endpoint to create a dataset
-#route for the batch processing that you can require a folder or zip for
-    
-    '''# Create a temporary file to save the uploaded QVD file
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        shutil.copyfileobj(file.file, tmp_file)
-        tmp_file_path = tmp_file.name
-    
-    # Now, use qvd_reader to read the QVD file into a pandas DataFrame
+"""
+In order to create a table in Power BI, we first need all of the columns and their data types.
+"""
+@app.post("/json_to_powerbi/")
+async def json_to_pbi_dataset(json_data: str):
     try:
-        df_read = qvd_reader.read(tmp_file_path)
-        df = pd.DataFrame(df_read)
+        original_data = json.loads(json_data)
+        first_row = next(iter(original_data.values()))
+
+        def infer_data_type(value):
+            if isinstance(value, int):
+                return "Int64"
+            elif isinstance(value, float):
+                return "Double"
+            elif isinstance(value, bool):
+                return "bool"
+            elif isinstance(value, str):
+                try:
+                    float(value)
+                    return "Double" if "." in value else "Int64"
+                except ValueError:
+                    return "DataTime" if "-" in value else "string"
+            else:
+                return "Unknown"
         
-        # Convert the DataFrame into JSON
-        json_data = df.to_json(orient="records")
-        
-        # Clean up the temporary file
-        os.unlink(tmp_file_path)
-        
-        return JSONResponse(content=json_data, media_type="application/json")
+        columns_list = [{"name": key, "dataType": infer_data_type(value)} for key, value in first_row.items()]
+
+        transformed_data = {
+            "name": "Orders", ## need to change this to infered from file name
+            "defaultMode": "Push",
+            "tables": [
+                {
+                    "name": "Table1",  ## need to change this to user input
+                    "columns": columns_list
+                }
+            ]
+        }
+
+        transformed_json_data = json.dumps(transformed_data, indent=4)
+        return transformed_json_data
+
+    except json.JSONDecodeError as e:
+        print("There was an error decoding the JSON:", e)
     except Exception as e:
-     os.unlink(tmp_file_path)
-     print(f"Error: {str(e)}")  # Add logging or printing for debugging
-     return JSONResponse(content={"error": str(e)}, status_code=500)
-     '''
+        print("An unexpected error occurred:", e)
